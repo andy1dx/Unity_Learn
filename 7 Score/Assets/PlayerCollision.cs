eb8d00465f464ef659ada2e472afa71656750705
Andy william <andy.william@bit-okutama.jp>
2018-12-07 23:23:41 +0900

    adding score unity file

---
+++ 7 Score/Assets/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public playerMovement movement;
+
+	// Use this for initialization
+	void OnCollisionEnter (Collision collisionInfo) {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+	}
+	
+
+}
